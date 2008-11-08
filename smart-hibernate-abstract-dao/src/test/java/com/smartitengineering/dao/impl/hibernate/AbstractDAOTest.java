8a93968afddb7b7cabac7093136d6382bda13d36
Imran M Yousuf <imyousuf@smartitengineering.com>
2008-11-08 12:14:14 +0600

    Add test case for domain creation
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- smart-hibernate-abstract-dao/src/test/java/com/smartitengineering/dao/impl/hibernate/AbstractDAOTest.java
+++ smart-hibernate-abstract-dao/src/test/java/com/smartitengineering/dao/impl/hibernate/AbstractDAOTest.java
@@ -18,8 +18,15 @@
  */
 package com.smartitengineering.dao.impl.hibernate;
 
+import com.smartitengineering.dao.impl.hibernate.domain.Author;
 import com.smartitengineering.dao.impl.hibernate.domain.Book;
+import com.smartitengineering.dao.impl.hibernate.domain.Publisher;
+import com.smartitengineering.domain.PersistentDTO;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import junit.framework.TestCase;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -29,7 +36,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class AbstractDAOTest
     extends TestCase {
-    
+
     private static ApplicationContext context;
 
     public AbstractDAOTest(String testName) {
@@ -40,7 +47,7 @@ public class AbstractDAOTest
     protected void setUp()
         throws Exception {
         super.setUp();
-        if(context == null) {
+        if (context == null) {
             context = new ClassPathXmlApplicationContext("app-context.xml");
         }
     }
@@ -56,7 +63,95 @@ public class AbstractDAOTest
      */
     public void testCreateEntity() {
         System.out.println("createEntity");
-        AbstractDAO<Book> instance = (AbstractDAO<Book>) context.getBean("testDao");
+        AbstractDAO<Book> bookInstance = getDaoInstance();
+        AbstractDAO<Author> authorInstance = getDaoInstance();
+        AbstractDAO<Publisher> publisherInstance = getDaoInstance();
+        /**
+         * Test null object creation, should throw IllegalArgumentException
+         */
+        try {
+            bookInstance.createEntity((Book) null);
+            fail("Should not proceed with saving NULL entity!");
+        }
+        catch (IllegalArgumentException argumentException) {
+        }
+        catch (Exception exception) {
+            fail(exception.getMessage());
+        }
+        /**
+         * Create proper test data.
+         * This set will contain single of everything - author, publisher, book
+         */
+        Publisher shebaProkashani = getShebaProkashani();
+        try {
+            publisherInstance.createEntity(shebaProkashani);
+            System.out.println(shebaProkashani.getId());
+        }
+        catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+        Author kaziAnowarHossain = getKaziAnowarHossain();
+        try {
+            authorInstance.createEntity(kaziAnowarHossain);
+            System.out.println(kaziAnowarHossain.getId());
+        }
+        catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+        Book book = getAgniSopoth(kaziAnowarHossain, shebaProkashani);
+        try {
+            bookInstance.createEntity(book);
+            System.out.println(book.getId());
+        }
+        catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+        /**
+         * Create multiple publications at once.
+         * One of the publications should have more than one book
+         */
+        Publisher oReilly = getOReilly();
+        Publisher annoProkash = getAnnoProkash();
+        try {
+            publisherInstance.createEntity(oReilly, annoProkash);
+            System.out.println(oReilly.getId() + ", " + annoProkash.getId());
+        }
+        catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+        /**
+         * Create multiple authors at once.
+         * There should be at least one book with multiple authors.
+         * A single author (set) should have multiple books.
+         */
+        Author brettMcLaugblin = getBrett();
+        Author davidLane = getDavidLane();
+        Author hughWilliams = getHughWilliams();
+        Author humayunAhmed = getHumayunAhmed();
+        try {
+            authorInstance.createEntity(brettMcLaugblin, davidLane, humayunAhmed,
+                hughWilliams);
+            System.out.println(brettMcLaugblin.getId() + ", " +
+                davidLane.getId() + ", " + humayunAhmed.getId() + ", " +
+                hughWilliams.getId());
+        }
+        catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+        Book kothaoKeoNei = getKothaoKeoNei(annoProkash, humayunAhmed);
+        Book agunerPoroshMoni = getAgunerPoroshMoni(annoProkash, humayunAhmed);
+        Book webDbApp = getWebDbApp(oReilly, davidLane, hughWilliams);
+        Book javaAndXml = getJavaAndXml(oReilly, brettMcLaugblin);
+        try {
+            bookInstance.createEntity(kothaoKeoNei, agunerPoroshMoni, webDbApp,
+                javaAndXml);
+            System.out.println(kothaoKeoNei.getId() + ", " +
+                agunerPoroshMoni.getId() + ", " + webDbApp.getId() + ", " +
+                javaAndXml.getId());
+        }
+        catch (Exception ex) {
+            fail(ex.getMessage());
+        }
     }
 
     /**
@@ -157,4 +252,120 @@ public class AbstractDAOTest
         System.out.println("readList");
     }
 
+    private Book getAgniSopoth(Author author,
+                               Publisher publisher) {
+        final String bookName = "Agni Sopoth";
+        final Date publishDate = new Date();
+        final String isbn = "123ABC";
+        return getBook(publisher, bookName, publishDate, isbn, author);
+    }
+
+    private Book getAgunerPoroshMoni(Publisher annoProkash,
+                                     Author humayunAhmed) {
+        return getBook(annoProkash, "Aguner Poroshmoni", new Date(), "222VFEE3",
+            humayunAhmed);
+    }
+
+    private Publisher getAnnoProkash() {
+        return getPublisher(new Date(), 50, "Anno Prokash");
+    }
+
+    private Author getBrett() {
+        return getAuthor("Brett McLaugblin", new Date());
+    }
+
+    private Author getDavidLane() {
+        return getAuthor("David Lane", new Date());
+    }
+
+    private Author getHughWilliams() {
+        return getAuthor("Hugh E. Williams", new Date());
+    }
+
+    private Author getHumayunAhmed() {
+        return getAuthor("Humayun Ahmed", new Date());
+    }
+
+    private Book getJavaAndXml(Publisher oReilly,
+                               Author brettMcLaugblin) {
+        return getBook(oReilly, "Java & XML", new Date(), "555UIP66",
+            brettMcLaugblin);
+    }
+
+    private Author getKaziAnowarHossain() {
+        final String name = "Kazi Anowar Hossain";
+        final Date birthDate = new Date();
+        return getAuthor(name, birthDate);
+    }
+
+    private Book getKothaoKeoNei(Publisher annoProkash,
+                                 Author humayunAhmed) {
+        return getBook(annoProkash, "Kothao Keo Nei", new Date(), "11134BCE",
+            humayunAhmed);
+    }
+
+    private Publisher getOReilly() {
+        return getPublisher(new Date(), 110, "O\'Reilly");
+    }
+
+    private Publisher getShebaProkashani() {
+        final String name = "Sheba Prokashoni";
+        final Date establishDate = new Date();
+        final int numOfEmployees = 100;
+        return getPublisher(establishDate, numOfEmployees, name);
+    }
+
+    private Publisher getPublisher(final Date establishDate,
+                                   final int numOfEmployees,
+                                   final String name) {
+        Publisher publisher =
+            new Publisher();
+        publisher.setEstablishedDate(establishDate);
+        publisher.setNumOfEmployees(numOfEmployees);
+        publisher.setName(name);
+        return publisher;
+    }
+
+    private Author getAuthor(final String name,
+                             final Date birthDate) {
+        Author author =
+            new Author();
+        author.setName(name);
+        author.setBirthDate(birthDate);
+        return author;
+    }
+
+    private Book getBook(final Publisher publisher,
+                         final String bookName,
+                         final Date publishDate,
+                         final String isbn,
+                         final Author... authors) {
+        Book book = new Book();
+        Set<Author> authorSet =
+            new HashSet<Author>();
+        for (Author author : authors) {
+            authorSet.add(author);
+        }
+        book.setAuthors(authorSet);
+        book.setPublisher(publisher);
+        book.setName(bookName);
+        book.setPublishDate(publishDate);
+        book.setIsbn(isbn);
+        return book;
+    }
+
+    private <T extends PersistentDTO<T>> AbstractDAO<T> getDaoInstance()
+        throws BeansException {
+        AbstractDAO<T> instance =
+            (AbstractDAO<T>) context.getBean("testDao");
+        assertNotNull(instance);
+        return instance;
+    }
+
+    private Book getWebDbApp(Publisher oReilly,
+                             Author davidLane,
+                             Author hughWilliams) {
+        return getBook(oReilly, "Web Database Applications", new Date(),
+            "444ERT6", davidLane, hughWilliams);
+    }
 }
