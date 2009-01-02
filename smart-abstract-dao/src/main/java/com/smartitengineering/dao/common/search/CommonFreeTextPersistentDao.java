d7f03898f45c7bdd1b13733dc0f96d7df0fa1ca6
Imran M Yousuf <imyousuf@smartitengineering.com>
2009-01-02 19:40:56 +0600

    Add API for Free text search engine integration
    This API will be the ground work in integrating free text search engine
    into Smart DAO. By default we will be supporting Compass Search
    Framework.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-abstract-dao/src/main/java/com/smartitengineering/dao/common/search/CommonFreeTextPersistentDao.java
@@ -0,0 +1,34 @@
+/*
+ * This is a common dao with basic CRUD operations and is not limited to any 
+ * persistent layer implementation
+ * 
+ * Copyright (C) 2008  Imran M Yousuf (imyousuf@smartitengineering.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.smartitengineering.dao.common.search;
+
+import com.smartitengineering.domain.PersistentDTO;
+
+/**
+ *
+ * @author imyousuf
+ */
+public interface CommonFreeTextPersistentDao<Template extends PersistentDTO> {
+
+    public void save(Template... data);
+
+    public void update(Template... data);
+
+    public void delete(Template... data);
+}
