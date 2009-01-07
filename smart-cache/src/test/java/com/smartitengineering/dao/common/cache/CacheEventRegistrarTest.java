0c041530a9aaadd03924bf58e47c92b5a121a62c
Imran M Yousuf <imyousuf@smartitengineering.com>
2009-01-07 13:56:23 +0600

    Make cache a separate module to ensure dependency are well modularized
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- smart-cache/src/test/java/com/smartitengineering/dao/common/cache/CacheEventRegistrarTest.java
+++ smart-cache/src/test/java/com/smartitengineering/dao/common/cache/CacheEventRegistrarTest.java
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package com.smartitengineering.dao.common;
+package com.smartitengineering.dao.common.cache;
 
 import com.smartitengineering.dao.common.cache.ChangeEvent;
 import com.smartitengineering.dao.common.cache.ChangeListener;
