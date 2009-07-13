cd40d861c316fde53a5be02ffbb37e445c6e0af4
Imran M Yousuf <imyousuf@smartitengineering.com>
2009-07-13 22:02:26 +0700

    Add XML implementation module for export and import
    Add basic quickstart artifact and add it to the parent POM.
    Remove the unnecessary dependency of DOM4j from XML-Impl module.
    Update distribution and site assemble scripts to include the new READMEs.
    Add exim modules' as doc's dependency to ensure execution order.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-exim/smart-exim-xml-impl/src/main/java/com/smartitengineering/exim/impl/xml/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.exim.impl.xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
