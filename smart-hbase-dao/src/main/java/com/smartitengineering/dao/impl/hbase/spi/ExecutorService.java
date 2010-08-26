8c978f53f1c7680fd527cae0b76b39660e6fd1bc
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-08-26 09:04:46 +0600

    Add SPI for executing HBase operations in contained environment
    The SPI basically abstracts the initialization of configuration, table pool
    and creation of table. It also abstracts the exception wrapping from an
    operation and returning a table to the pool. Thus allowing clients to
    specifically concentrate on the main operation rather than working on its
    surroundings. It uses callback handler to allow users to plugin their main
    actions.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-hbase-dao/src/main/java/com/smartitengineering/dao/impl/hbase/spi/ExecutorService.java
@@ -0,0 +1,38 @@
+/*
+ * This is a common dao with basic CRUD operations and is not limited to any
+ * persistent layer implementation
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.smartitengineering.dao.impl.hbase.spi;
+
+import org.apache.hadoop.hbase.client.HTablePool;
+
+/**
+ * An API for executing HBase requests.
+ * @author imyousuf
+ */
+public interface ExecutorService {
+
+  /**
+   * Execute a HBase request and use the callback to perform the custom part and return if needed.
+   * @param <ReturnType> The return type of the execution, may be {@link Void}
+   * @param tableName Name of the table the callback will perform action on. May be blank, check implementation for
+   *                  details
+   * @param callback The custom action callback
+   * @return Return whatever is wished from the callback to the executor.
+   */
+  public <ReturnType> ReturnType execute(String tableName, Callback<ReturnType> callback);
+}
