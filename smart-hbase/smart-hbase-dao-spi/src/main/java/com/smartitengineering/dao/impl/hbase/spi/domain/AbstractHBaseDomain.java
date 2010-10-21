e10811729efa423ba485fe88645b52451ec72b69
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-10-21 10:03:48 +0600

    Add abstract domain for HBase with Long version specified
    The main reason for abstraction is to ease generics usage for user
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-hbase/smart-hbase-dao-spi/src/main/java/com/smartitengineering/dao/impl/hbase/spi/domain/AbstractHBaseDomain.java
@@ -0,0 +1,31 @@
+/*
+ * This is a common dao with basic CRUD operations and is not limited to any
+ * persistent layer implementation
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.smartitengineering.dao.impl.hbase.spi.domain;
+
+import com.smartitengineering.domain.AbstractGenericPersistentDTO;
+import com.smartitengineering.domain.PersistentDTO;
+import java.io.Serializable;
+
+/**
+ *
+ * @author imyousuf
+ */
+public abstract class AbstractHBaseDomain<Template extends PersistentDTO, IdType extends Comparable<IdType> & Serializable>
+    extends AbstractGenericPersistentDTO<Template, IdType, Long> {
+}
