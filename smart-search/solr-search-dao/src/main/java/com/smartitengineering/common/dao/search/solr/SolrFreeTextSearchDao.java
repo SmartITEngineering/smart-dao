b7dd98a36e778bb613e156161fd0f76320823322
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-10-23 14:39:05 +0600

    Implement simple search dao for solr
    It expects string like param and expect it to be query string, if the
    name is 'q' and if any other name it willa add it as 'set' to query.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-search/solr-search-dao/src/main/java/com/smartitengineering/common/dao/search/solr/SolrFreeTextSearchDao.java
@@ -0,0 +1,132 @@
+/*
+ * This is a common dao with basic CRUD operations and is not limited to any
+ * persistent layer implementation
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.smartitengineering.common.dao.search.solr;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.smartitengineering.common.dao.search.CommonFreeTextSearchDao;
+import com.smartitengineering.dao.common.queryparam.Order;
+import com.smartitengineering.dao.common.queryparam.QueryParameter;
+import com.smartitengineering.dao.common.queryparam.QueryParameterCastHelper;
+import com.smartitengineering.dao.common.queryparam.SimpleNameValueQueryParameter;
+import com.smartitengineering.dao.common.queryparam.StringLikeQueryParameter;
+import com.smartitengineering.dao.solr.MultivalueMap;
+import com.smartitengineering.dao.solr.SolrQueryDao;
+import com.smartitengineering.util.bean.adapter.GenericAdapter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrQuery.ORDER;
+
+/**
+ *
+ * @author imyousuf
+ */
+public class SolrFreeTextSearchDao<T> implements CommonFreeTextSearchDao<T> {
+
+  @Inject
+  private GenericAdapter<T, MultivalueMap<String, Object>> adapter;
+  @Inject
+  private SolrQueryDao queryDao;
+  @Inject
+  private ExecutorService executorService;
+  @Inject
+  @Named("waitTime")
+  private long waitTime;
+  @Inject
+  @Named("waitTimeUnit")
+  private TimeUnit waitTimeUnit;
+
+  @Override
+  public Collection<T> search(List<QueryParameter> parameters) {
+    SolrQuery query = new SolrQuery();
+    for (QueryParameter param : parameters) {
+      switch (param.getParameterType()) {
+        case PARAMETER_TYPE_PROPERTY:
+          StringLikeQueryParameter queryParameter =
+                                   QueryParameterCastHelper.STRING_PARAM_HELPER.cast(param);
+          if (queryParameter.getPropertyName().equals("q")) {
+            String queryString = queryParameter.getValue();
+            query.setQuery(queryString);
+          }
+          else {
+            query.add(queryParameter.getPropertyName(), queryParameter.getValue());
+          }
+          break;
+        case PARAMETER_TYPE_ORDER_BY:
+          SimpleNameValueQueryParameter<Order> parameter = QueryParameterCastHelper.SIMPLE_PARAM_HELPER.cast(param);
+          ORDER order = parameter.getValue().equals(Order.ASC) ? ORDER.asc : ORDER.desc;
+          query.setSortField(parameter.getPropertyName(), order);
+          break;
+        default:
+          throw new UnsupportedOperationException("Only property and order by query parameter is supported!");
+      }
+    }
+    final List<MultivalueMap<String, Object>> result = queryDao.getResult(query);
+    return adapter.convertInversely(result.toArray(new MultivalueMap[result.size()]));
+  }
+
+  @Override
+  public Collection<T> search(QueryParameter... parameters) {
+    return search(Arrays.asList(parameters));
+  }
+
+  public GenericAdapter<T, MultivalueMap<String, Object>> getAdapter() {
+    return adapter;
+  }
+
+  public void setAdapter(GenericAdapter<T, MultivalueMap<String, Object>> adapter) {
+    this.adapter = adapter;
+  }
+
+  public ExecutorService getExecutorService() {
+    return executorService;
+  }
+
+  public void setExecutorService(ExecutorService executorService) {
+    this.executorService = executorService;
+  }
+
+  public SolrQueryDao getQueryDao() {
+    return queryDao;
+  }
+
+  public void setQueryDao(SolrQueryDao queryDao) {
+    this.queryDao = queryDao;
+  }
+
+  public long getWaitTime() {
+    return waitTime;
+  }
+
+  public void setWaitTime(long waitTime) {
+    this.waitTime = waitTime;
+  }
+
+  public TimeUnit getWaitTimeUnit() {
+    return waitTimeUnit;
+  }
+
+  public void setWaitTimeUnit(TimeUnit waitTimeUnit) {
+    this.waitTimeUnit = waitTimeUnit;
+  }
+}
