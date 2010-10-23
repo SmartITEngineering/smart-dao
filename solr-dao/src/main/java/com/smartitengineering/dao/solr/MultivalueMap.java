11ffefd86e28944430900ff53c50df7998644f15
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-10-23 11:11:31 +0600

    Add Solr API for search
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ solr-dao/src/main/java/com/smartitengineering/dao/solr/MultivalueMap.java
@@ -0,0 +1,33 @@
+/*
+ * This is a common dao with basic CRUD operations and is not limited to any
+ * persistent layer implementation
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.smartitengineering.dao.solr;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author imyousuf
+ */
+public interface MultivalueMap<K, V> extends Map<K, List<V>> {
+
+  void addValue(K key, V value);
+
+  <T extends K> V getFirst(K key);
+}
